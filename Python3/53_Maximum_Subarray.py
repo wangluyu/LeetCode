c5c40fddc2c836479ffacb8f4a4e8987bc0b9493
wangluyu <13728742101@163.com>
2020-01-14 19:34:50 +0800

    python 53. 最大子序和

---
+++ Python3/53_Maximum_Subarray.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Desc: 53. 最大子序和 (简单)
+Author: wangluyu
+Date: 2020/1/14
+"""
+from typing import List
+
+
+class Solution:
+    """
+    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+    Example:
+    Input: [-2,1,-3,4,-1,2,1,-5,4],
+    Output: 6
+    Explanation: [4,-1,2,1] has the largest sum = 6.
+    Follow up:
+    If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+    给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+    示例:
+    输入: [-2,1,-3,4,-1,2,1,-5,4],
+    输出: 6
+    解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+    进阶:
+    如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+    """
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = nums[0]
+        curr_sum = 0
+        for i in range(len(nums)):
+            curr_sum = max(nums[i], curr_sum + nums[i])
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
+
+
+if __name__ == '__main__':
+    nums = [-2,1,-3,4,-1,2,1,-5,4]
+    s=Solution()
+    print(s.maxSubArray(nums))
