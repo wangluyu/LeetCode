b17f5118a1d4e6948acacdd615a63d4908b4db7b
wangluyu <13728742101@163.com>
2020-01-15 11:34:01 +0800

    Go 58. 最后一个单词的长度

---
+++ Golang/58LengthOfLastWord.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+58. 最后一个单词的长度 (简单)
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
+If the last word does not exist, return 0.
+Note: A word is defined as a maximal substring consisting of non-space characters only.
+Example:
+Input: "Hello World"
+Output: 5
+给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。
+如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+如果不存在最后一个单词，请返回 0 。
+说明：一个单词是指仅由字母组成、不包含任何空格的 最大子字符串。
+示例:
+输入: "Hello World"
+输出: 5
+*/
+func lengthOfLastWord(s string) int {
+	l := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if string(s[i]) != " " {
+			l += 1
+		} else if l != 0 {
+			return l
+		}
+	}
+	return l
+}
+
+func main() {
+	s := "dadsa "
+	fmt.Println(lengthOfLastWord(s))
+}
